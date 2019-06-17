e43aa4ff72ca33d95436b288ae9dc5fd458c257d
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-06-18 00:22:17 +0200

    1. Sviluppato db giocatori

---
+++ datab.php
@@ -0,0 +1,49 @@
+<?php
+$arrayGiocatori=[];
+
+//Funzione che mi generi il codice del giocatore
+function generateRandomCode($lettereCodice,$numeriCodice){
+  $codiceProvvisorio='';
+
+  for ($i=0; $i < $lettereCodice ; $i++) {
+    $codiceProvvisorio = $codiceProvvisorio . chr(rand(97,122));
+  }
+
+  $codiceGiocatore = strtoupper($codiceProvvisorio);
+
+  for ($i=0; $i < $numeriCodice; $i++) {
+    $codiceGiocatore .= rand(0,9);
+  }
+  return $codiceGiocatore;
+};
+
+//Funzione che mi calcola un dato relativo a punti, falli e rimbalzi in maniera casuale
+function generatePointReboundsFauls($min,$max){
+  return rand($min,$max);
+};
+
+$g;
+for ($g=0; $g < 100; $g++) {
+  do {
+    $giocatore=[
+      "codiceGiocatore"=> generateRandomCode(3,3),
+      "puntiFatti"=> generatePointReboundsFauls(1,50),
+      "Rimbalzi"=> generatePointReboundsFauls(1,20),
+      "falli"=> generatePointReboundsFauls(1,12),
+      "tiri2"=> generatePointReboundsFauls(1,52),
+      "tiri3"=> generatePointReboundsFauls(1,40)
+    ];
+  } while (in_array($giocatore["codiceGiocatore"],$arrayGiocatori));
+
+  $arrayGiocatori[]=$giocatore;
+}
+?>
+<select id="player_list">
+  <?php
+
+  foreach ($arrayGiocatori as $key => $value) {
+    echo "<option>" . $value["codiceGiocatore"] . "</option>";
+  }
+  ?>
+  <option value=""disabled selected hidden> Scegli il Codice </option>
+</select>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'datab.php'?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP_Basket_AR</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    
+  </body>
+</html>
